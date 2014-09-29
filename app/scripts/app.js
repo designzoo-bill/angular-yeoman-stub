b34aec10df84879fa2b08cb2aa594d99c704fb8a
Bill Morrison <bill@thedesignzoo.co.uk>
2014-09-29 22:37:05 +0100

    initial

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name testApp
+ * @description
+ * # testApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('testApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/terms', {
+        templateUrl: 'views/terms.html',
+        controller: 'TermsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/terms.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testApp.controller:TermsCtrl
+ * @description
+ * # TermsCtrl
+ * Controller of the testApp
+ */
+angular.module('testApp')
+  .controller('TermsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
