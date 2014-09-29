0df3bcd20fdfadb6f142e6dd6c8eabbc6c562987
Bill Morrison <bill@thedesignzoo.co.uk>
2014-09-29 23:53:03 +0100

    test

--- web.js
+++ web.js
@@ -3,15 +3,20 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
+/* At the top, with other redirect methods before other routes */
+app.get('*',function(req,res,next){
+
+  if(req.headers['x-forwarded-proto']!='https') {
+
+  	res.redirect('https://ancient-retreat-8481.herokuapp.com/'+req.url);
+  }
+  else {
+
+  	//next(); /* Continue to other routes if we're not redirecting */
+  }
+})
+
 //app.use(express.logger('dev'));
 app.use(morgan('combined'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
-
-function ensureSec(req, res, next) {
-    if (req.headers['x-forwarded-proto'] == 'https') { 
-        return next(); 
-    } else {
-        res.redirect('https://' + req.headers.host + req.path);
-    }
-}
