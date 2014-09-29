eb51989b005b1ef7ef4b806b425ab635de0c2998
Bill Morrison <bill@thedesignzoo.co.uk>
2014-09-30 00:28:26 +0100

    test
    test

--- web.js
+++ web.js
@@ -3,6 +3,33 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
+console.log("env: ", process.env.NODE_ENV);
+
+/*if (process.env.NODE_ENV == 'production') {
+
+	is_secure = function (req) {req.headers['x-forwarded-proto'] == 'https';};
+}
+  
+else {
+
+	is_secure = function (req){req.secure;};
+}
+  
+
+var redirect_to_https = function(req, res, next) {
+
+  if (!is_secure(req)) {
+
+  	res.redirect("http://ancient-retreat-8481.herokuapp.com/" + req.url);
+  } 
+  else {
+
+  	next();
+  }
+};
+
+app.use(redirect_to_https);*/
+
 //app.use(express.logger('dev'));
 app.use(morgan('combined'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
