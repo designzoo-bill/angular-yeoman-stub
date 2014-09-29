7d3e676ac4ef82332450a03ff846b2b2e0c2dda7
Bill Morrison <bill@thedesignzoo.co.uk>
2014-09-30 00:24:39 +0100

    test

--- web.js
+++ web.js
@@ -18,7 +18,7 @@ var redirect_to_https = function(req, res, next) {
 
   if (!is_secure(req)) {
 
-  	res.redirect("https://ancient-retreat-8481.herokuapp.com/" + req.url);
+  	res.redirect("http://ancient-retreat-8481.herokuapp.com/" + req.url);
   } 
   else {
 
