a3ec3833c18797e687e7337f5e1d0248c8235b0b
Bill Morrison <bill@thedesignzoo.co.uk>
2014-09-30 00:14:06 +0100

    test

--- web.js
+++ web.js
@@ -3,30 +3,22 @@ var express = require('express');
 var morgan = require('morgan');
 var app = express();
 
-var http = require('http');
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if process.env.NODE_ENV == 'production'
+  is_secure = (req) ->
+    req.headers['x-forwarded-proto'] == 'https'
+else
+  is_secure = (req) -> req.secure
 
-var forceSsl = function (req, res, next) {
+redirect_to_https = (req, res, next) ->
+  if not is_secure(req)
+    res.redirect "htts://google.com" + req.url
+  else
+    next()
 
-	if (req.headers['x-forwarded-proto'] !== 'https') {
+app
+  .use(redirect_to_https)
 
-		return res.redirect(['https://', req.get('Host'), req.url].join(''));
-	} 
-	else {
-
-		next();
-	}
-};
-
-app.handle(function () {
-
-    if (env === 'production') {
-
-        app.use(forceSsl);
-    }
-
-	//app.use(express.logger('dev'));
-	app.use(morgan('combined'));
-	app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-	app.listen(process.env.PORT || 5000);
-});
+//app.use(express.logger('dev'));
+app.use(morgan('combined'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
