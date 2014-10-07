ef1d373d67888503e88d2afaabf1ce593ab3d4a9
Bill Morrison <bill@thedesignzoo.co.uk>
2014-10-07 23:57:03 +0100

    test

---
+++ app/scripts/controllers/users.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testApp.controller:UsersCtrl
+ * @description
+ * # UsersCtrl
+ * Controller of the testApp
+ */
+angular.module('testApp')
+  .controller('UsersCtrl', function ($scope, UserData) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.users = UserData;
+  });
