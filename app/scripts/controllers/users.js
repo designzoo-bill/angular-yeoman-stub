c58d4cd0466f53aeb36549f2f28836637c12a9b0
Bill Morrison <bill@thedesignzoo.co.uk>
2014-10-09 22:03:32 +0100

    env test 2

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -1,9 +1,15 @@
 'use strict';
 
-angular.module('testApp').controller('UsersCtrl', function ($scope, UserData) {
+angular.module('testApp').controller('UsersCtrl', function ($scope, UserData, $http) {
 
   $scope.users = UserData;
 
   // set the default order by
   $scope.predicate = 'name.last';
+
+  // get the env
+  $scope.env = 'local';
+  $http.get('/nodeenv').success(function(data){
+  	$scope.env = data.env;
+  });
 });
