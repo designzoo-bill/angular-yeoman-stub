94c641710351416eb3aa2b0a54576f1038fcda49
Bill Morrison <bill@thedesignzoo.co.uk>
2014-10-13 21:35:32 +0100

    build

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,31 +15,16 @@ angular.module('testApp').controller('MainCtrl', function ($scope, $routeParams,
 
  	$scope.$watch('todos', function (newValue, oldValue) {
 
- 		$scope.remainingCount = $filter('filter')(todos, { completed: false }).length;
- 		$scope.completedCount = todos.length - $scope.remainingCount;
- 		$scope.allChecked = !$scope.remainingCount;
-
 		if (newValue !== oldValue) { // This prevents unneeded calls to the local storage
 
 			todoStorage.put(todos);
 		}
 	}, true);
 
-	// Monitor the current route for changes and adjust the filter accordingly.
-	$scope.$on('$routeChangeSuccess', function () {
-
-		var status = $scope.status = $routeParams.status || '';
-
-		$scope.statusFilter = (status === 'active') ?
-		{ completed: false } : (status === 'completed') ?
-		{ completed: true } : null;
-	});
-
 	$scope.addTodo = function () {
 
-		console.log('addTodod');
-
 		var newTodo = $scope.newTodo.trim();
+		var orderBy = todos.length;
 
 		if (!newTodo.length) {
 
@@ -49,55 +34,21 @@ angular.module('testApp').controller('MainCtrl', function ($scope, $routeParams,
 		todos.push({
 
 			title: newTodo,
-			completed: false
+			completed: false,
+			orderBy: orderBy
 		});
 
 		$scope.newTodo = '';
 	};
 
-	$scope.editTodo = function (todo) {
+	$scope.toggleTodo = function (todo) {
 
-		$scope.editedTodo = todo;
-
-		// Clone the original todo to restore it on demand.
-		$scope.originalTodo = angular.extend({}, todo);
-	};
-
-	$scope.doneEditing = function (todo) {
-
-		$scope.editedTodo = null;
-		todo.title = todo.title.trim();
-
-		if (!todo.title) {
-
-			$scope.removeTodo(todo);
-		}
-	};
-
-	$scope.revertEditing = function (todo) {
-
-		todos[todos.indexOf(todo)] = $scope.originalTodo;
-		$scope.doneEditing($scope.originalTodo);
+		console.log('toggleTodo');
+		todo.completed = !todo.completed;
 	};
 
 	$scope.removeTodo = function (todo) {
 
 		todos.splice(todos.indexOf(todo), 1);
-	};
-
-	$scope.clearCompletedTodos = function () {
-
-		$scope.todos = todos = todos.filter(function (val) {
-
-			return !val.completed;
-		});
-	};
-
-	$scope.markAll = function (completed) {
-
-		todos.forEach(function (todo) {
-
-			todo.completed = !completed;
-		});
 	};
 });
