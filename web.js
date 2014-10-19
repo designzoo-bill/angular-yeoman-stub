3f05b4c8471b4ccd4ec56f09fb077b4309bc6123
Bill Morrison <bill@thedesignzoo.co.uk>
2014-10-19 18:18:14 +0100

    test

--- web.js
+++ web.js
@@ -12,9 +12,9 @@ app.get('/nodeenv', function(req, res, next){
 });
 
 // rewrite for html5 mode to work
-app.use('/scripts', express.static(__dirname + '/scripts'));
-app.use('/dist', express.static(__dirname + '/dist'));
-app.use('/styles', express.static(__dirname + '/styles'));
+//app.use('/scripts', express.static(__dirname + '/scripts'));
+//app.use('/dist', express.static(__dirname + '/dist'));
+//app.use('/styles', express.static(__dirname + '/styles'));
 //app.use('/partials', express.static(__dirname + '/partials'));
 
 app.all('/*', function(req, res, next) {
@@ -23,7 +23,7 @@ app.all('/*', function(req, res, next) {
     res.sendfile('index.html', { root: __dirname });
 });
 
-app.listen(3006); //the port you want to use
+//app.listen(3006); //the port you want to use
 
 /*if (process.env.NODE_ENV === 'production') {
 
