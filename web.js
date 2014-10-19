eec5a8a23271fcab7d1044dea7417459f59aae0d
Bill Morrison <bill@thedesignzoo.co.uk>
2014-10-19 18:46:45 +0100

    tes

--- web.js
+++ web.js
@@ -53,7 +53,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('*', function(req, res) {
-  res.redirect('index.html');
+  res.redirect('index');
 });
 
 //app.listen(3006); //the port you want to use
