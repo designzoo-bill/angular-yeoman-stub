1559b8e82bedbe68b0eeb8649045bf0247813653
Bill Morrison <bill@thedesignzoo.co.uk>
2014-10-19 20:16:09 +0100

    test

--- web.js
+++ web.js
@@ -53,7 +53,7 @@ app.get('views/:name', function (req, res) {
 
 app.all('/*', function(req, res, next) {
     // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: __dirname});
+    res.sendFile('index.html');
 });
 
 /*app.get('/', function(req, res) {
