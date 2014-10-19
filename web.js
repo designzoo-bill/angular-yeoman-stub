ac0ad10eeaa8febddb97f40b915d1c4ea31a4f4f
Bill Morrison <bill@thedesignzoo.co.uk>
2014-10-19 18:06:32 +0100

    test

--- web.js
+++ web.js
@@ -12,10 +12,10 @@ app.get('/nodeenv', function(req, res, next){
 });
 
 // rewrite for html5 mode to work
-app.use('/scripts', express.static(__dirname + '/scripts'));
+/*app.use('/scripts', express.static(__dirname + '/scripts'));
 app.use('/dist', express.static(__dirname + '/../dist'));
 app.use('/styles', express.static(__dirname + '/styles'));
-app.use('/partials', express.static(__dirname + '/partials'));
+app.use('/partials', express.static(__dirname + '/partials'));*/
 
 app.all('/*', function(req, res, next) {
     // Just send the index.html for other files to support HTML5Mode
