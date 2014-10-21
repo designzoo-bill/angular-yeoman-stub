3f85f039e2e311cd9953df8c1175cd9b4ddf9e9e
Bill Morrison <bill@thedesignzoo.co.uk>
2014-10-21 21:10:18 +0100

    trying to get expressjs rewrite working

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var morgan = require('morgan');
-//var routes = require('./routes');
 var app = express();
+
+// Routes - to support HTML5Mode - Start
 
 // api call for client to get the env
 app.get('/node-env', function(req, res, next){
@@ -12,13 +13,11 @@ app.get('/node-env', function(req, res, next){
 
 app.get('/:name', function(req, res, next) {
 
-    // Just send the index.html for other files to support HTML5Mode
     res.sendFile('index.html', {root: './dist/'});
 });
 
 app.all('/', function(req, res, next) {
 
-    // Just send the index.html for other files to support HTML5Mode
     res.sendFile('index.html', {root: './dist/'});
 });
 
@@ -46,133 +45,8 @@ app.get('/images/:name', function (req, res) {
   res.sendFile(name, {root: './dist/images/'});
 });
 
-// Routes
-
-//app.get('/', routes.index);
-//app.get('/home', routes.index);
-
-//app.get('/partials/:name', routes.partials);
-
-/*app.engine('html', require('html').renderFile);
-app.set('view engine', 'html');*/
-
-//app.get('/', routes.index);
-//app.get('*', routes.index);
-
-//var path = require('path');
-
-//app.use(app.router);
-/*app.use(function(req, res) {
-  // Use res.sendfile, as it streams instead of reading the file into memory.
-  res.sendFile('index.html', {root: './'});
-});
-
-// api call for client to get the env
-app.get('/node-env', function(req, res, next){
-	
-    return res.json({ env: app.get('env') });
-});*/
-
-/********
- * Routes
- ********/
- 
-// serve index and view partials
-//app.get('/', routes.index);
-//app.get('/views/:name', routes.views);
- 
-// redirect all others to the index (HTML5 history)
-//app.get('*', routes.index);
-
-// rewrite for html5 mode to work
-/*app.use('/scripts', express.static(__dirname + '/scripts'));
-//app.use('/dist', express.static(__dirname + '/../dist'));
-app.use('/styles', express.static(__dirname + '/styles'));
-//app.use('/partials', express.static(__dirname + '/partials'));
-
-/*app.get('bower_components/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('bower_components/' + name);
-});
-
-app.get('images/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('images/' + name);
-});
-
-app.get('scripts/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('scripts/' + name);
-});*/
-
-/*app.get('styles/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('styles/' + name);
-});*/
-
-/*app.get('views/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('views/' + name);
-});
-*/
-/*app.all('/*', function(req, res, next) {
-    // Just send the index.html for other files to support HTML5Mode
-    res.sendFile('index.html', { root: __dirname+'../dist/'});
-});*/
-
-/*app.configure(function() {
-  app.use('/', express.static(__dirname + '/'));
-});*/
-
-/*app.get('/styles/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('styles/' + name);
-});*/
-
-/*app.get('/views/:name', function (req, res) {
-  var name = req.params.name;
-  res.render('views/' + name);
-});*/
-
-/*app.get('*', function(request, response, next) {
-  response.sendFile(__dirname + '/dist/index.html');
-});*/
-
-/*app.get('/', function(req, res) {
-  res.render('index');
-});*/
-
-/*app.get('*', function(req, res) {
-  res.redirect('/');
-});*/
-
-//app.listen(3006); //the port you want to use
-
-/*if (process.env.NODE_ENV === 'production') {
-
-	is_secure = function (req) {req.headers['x-forwarded-proto'] === 'https';};
-}
-
-else {
-
-	is_secure = function (req){req.secure;};
-}
-
-
-var redirect_to_https = function(req, res, next) {
-
-	if (!is_secure(req)) {
-
-		res.redirect("https://ancient-retreat-8481.herokuapp.com/" + req.url);
-	} 
-	else {
-
-		next();
-	}
-};
-
-app.use(redirect_to_https);*/
+// Routes - End
 
 app.use(morgan('dev'));
-//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
